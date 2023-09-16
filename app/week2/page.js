f941e892f1ee8a271a6b8dbdbe9f781fdbe1cc1e
KH-Tom <116770365+KH-Tom@users.noreply.github.com>
2023-09-16 13:34:25 -0600

    publish to vercel

--- app/week2/page.js
+++ app/week2/page.js
@@ -6,6 +6,7 @@ export default function Home2() {
       <main>
         <h1>page2.1</h1>
         <h2>page2.2</h2>
+        <h3>publish to vercel</h3>
       </main>
       <Link href="/">Home</Link>
     </>
